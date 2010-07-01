2b17d3b199722ba8a470211564df35dcb4d41395
cohitre <carlosrr@gmail.com>
2010-06-30 22:10:57 -0700

    Disabling the comments js for now. Changing copyright information.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,31 @@
-$.initialize("#note-text", function () {
-  var citationFormUrl = $("meta[name=new-citation]").attr('content');
-  
-  this.bind('textselect', function(event, string, element, range){
-    
-    
-    if ( !range.isCollapsed ) {
-      var offset = $(this).rangeOffset(range);
-    
-      $.facebox(function(){
-        $.get(citationFormUrl, function(form){
-          var self = $(form);
-          self
-            .find('#selected-text').text(string).end()
-            .find('#citation_range_begin').val(offset.start).end()
-            .find('#citation_range_end').val(offset.end);
-          $.facebox(self);
-        })
-      });
-    }
-  });
-});
-
-$.initialize('.citation', function () {
-  this
-    .filter(function(){
-      return $(this).attr('data-citations') !== "";
-    })
-    .each(function(index,item){
-      $(item).addClass('note-color-'+index%8);
-    });
-});
+// Turning this off for now.
+// $.initialize("#note-text", function () {
+//   var citationFormUrl = $("meta[name=new-citation]").attr('content');
+//   
+//   this.bind('textselect', function (event, string, element, range) {
+//     if ( !range.isCollapsed ) {
+//       var offset = $(this).rangeOffset(range);
+//     
+//       $.facebox(function () {
+//         $.get(citationFormUrl, function (form) {
+//           $.facebox(
+//             $(form)
+//               .find('#selected-text').text(string).end()
+//               .find('#citation_range_begin').val(offset.start).end()
+//               .find('#citation_range_end').val(offset.end).end()
+//           );
+//         });
+//       });
+//     }
+//   });
+// });
+// 
+// $.initialize('.citation', function () {
+//   this
+//     .filter(function(){
+//       return $(this).attr('data-citations') !== "";
+//     })
+//     .each(function(index,item){
+//       $(item).addClass('note-color-'+index%8);
+//     });
+// });
